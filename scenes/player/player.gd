ded1e49ac2bcc5913c6a65d27aaa487a29e2845f
Maxington20 <max.herrington@gmail.com>
2026-09-08 15:01:48 -0400

    adjustments to movement.

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -4,6 +4,8 @@ extends CharacterBody2D
 const GRAVITY := 1000.0
 const SPEED := 200.0
 const JUMP_VELOCITY := -400.0
+const ACCELERATION := 100 
+const DECELERATION := 100
 
 func _physics_process(delta: float) -> void:
 	
@@ -15,6 +17,10 @@ func _physics_process(delta: float) -> void:
 	
 	var direction := Input.get_axis("move_left", "move_right")
 	
-	velocity.x = SPEED * direction
+	 ## velocity.x = SPEED * direction
+	if direction != 0:
+		velocity.x = move_toward(velocity.x, direction * SPEED, ACCELERATION * delta)
+	else:
+		velocity.x = move_toward(velocity.x, 0, DECELERATION * delta)
 	
 	move_and_slide()
